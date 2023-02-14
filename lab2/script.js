78c542b1b32c4bdbc014dedad94d59957797578d
Nikolay Gorkunov <nichto77@gmail.com>
2023-02-14 15:18:09 +0300

    lab2 done

---
+++ lab2/script.js
@@ -0,0 +1,104 @@
+// файл script.js
+window.onload = function(){ 
+
+    let a = ''
+    let b = ''
+    let expressionResult = ''
+    let selectedOperation = null
+    
+    // окно вывода результата
+    outputElement = document.getElementById("result")
+    
+    // список объектов кнопок циферблата (id которых начинается с btn_digit_)
+    digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]')
+    
+    function onDigitButtonClicked(digit) {
+        if (!selectedOperation) {
+            if ((digit != '.') || (digit == '.' && !a.includes(digit))) { 
+                a += digit
+            }
+            outputElement.innerHTML = a
+        } else {
+            if ((digit != '.') || (digit == '.' && !b.includes(digit))) { 
+                b += digit
+                outputElement.innerHTML = b        
+            }
+        }
+    }
+    
+    // устанавка колбек-функций на кнопки циферблата по событию нажатия
+    digitButtons.forEach(button => {
+        button.onclick = function() {
+            const digitValue = button.innerHTML
+            onDigitButtonClicked(digitValue)
+        }
+    });
+    
+    // установка колбек-функций для кнопок операций
+    document.getElementById("btn_op_mult").onclick = function() { 
+        if (a === '') return
+        selectedOperation = 'x'
+    }
+    document.getElementById("btn_op_plus").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '+'
+    }
+    document.getElementById("btn_op_minus").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '-'
+    }
+    document.getElementById("btn_op_div").onclick = function() { 
+        if (a === '') return
+        selectedOperation = '/'
+    }
+    
+    // кнопка очищения
+    document.getElementById("btn_op_clear").onclick = function() { 
+        a = ''
+        b = ''
+        selectedOperation = ''
+        expressionResult = ''
+        outputElement.innerHTML = 0
+    }
+    
+    // кнопка расчёта результата
+    document.getElementById("btn_op_equal").onclick = function() { 
+        if (a === '' || b === '' || !selectedOperation)
+            return
+            
+        switch(selectedOperation) { 
+            case 'x':
+                expressionResult = (+a) * (+b)
+                break;
+            case '+':
+                expressionResult = (+a) + (+b)
+                break;
+            case '-':
+                expressionResult = (+a) - (+b)
+                break;
+            case '/':
+                expressionResult = (+a) / (+b)
+                break;
+        }
+        
+        a = expressionResult.toString()
+        b = ''
+        selectedOperation = null
+    
+        outputElement.innerHTML = a
+    }
+
+    document.getElementById("btn_op_bspace").onclick = function() {     // !!! Мой вариант 3
+        let len = 0
+        if (!selectedOperation) {
+            len = a.length
+            if (len > 0) a = a.slice(0, len - 1)
+            outputElement.innerHTML = a
+        } else {
+            len = b.length
+            if (len > 0) b = b.slice(0, len - 1)
+            outputElement.innerHTML = b        
+        }
+    }
+};
+    
